4bd40a225241015f851ee6de7a42e3621cc2a1d2
Lisa Klein <lisa.t.klein@hotmail.de>
2021-04-02 15:23:56 +0200

    Window Events

--- TowerDelivery/src/TowerDelivery/Application.h
+++ TowerDelivery/src/TowerDelivery/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "TowerDelivery/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace TowerDelivery {
@@ -14,7 +15,11 @@ namespace TowerDelivery {
 		
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};

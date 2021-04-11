ae42b47abf73ae54d009fc5cc3a1b6c1579d8dd2
Lollobin <e11909390@student.tuwien.ac.at>
2021-04-11 21:39:32 +0200

    Buffer Klassen erstellt

---
+++ TowerDelivery/src/TowerDelivery/Rendering/Buffer.cpp
@@ -0,0 +1,49 @@
+#include "tdpch.h"
+#include "Buffer.h"
+
+#include "glad/glad.h"
+
+namespace TowerDelivery {
+	//Vertex Buffer
+
+	VertexBuffer::VertexBuffer(float* vertices, uint32_t size) {
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+	}
+
+	VertexBuffer::~VertexBuffer() {
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void VertexBuffer::Bind() const {
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void VertexBuffer::Unbind() const {
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	//Index Buffer
+
+	IndexBuffer::IndexBuffer(float* indices, uint32_t count)
+		: m_Count(count)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	}
+
+	IndexBuffer::~IndexBuffer() {
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void IndexBuffer::Bind() const {
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void IndexBuffer::Unbind() const {
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+}

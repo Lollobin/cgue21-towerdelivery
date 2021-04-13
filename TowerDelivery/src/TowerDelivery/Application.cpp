611c85bc99a0fe2be94cfe1b9cb0682e99f580be
LisaTaimi <e11905185@student.tuwien.ac.at>
2021-04-13 15:58:27 +0200

    draws cube instead of triangle; Quick changes: vertex Array uv gelöscht; vertex Array werden float verktoren übergeben

--- TowerDelivery/src/TowerDelivery/Application.cpp
+++ TowerDelivery/src/TowerDelivery/Application.cpp
@@ -82,34 +82,113 @@ namespace TowerDelivery {
 		btDiscreteDynamicsWorld* dynamics_world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collision_configuration);
 		dynamics_world->setGravity(btVector3(0, -10, 0));
 		*/
-		std::vector<glm::vec3> positions{
-			glm::vec3(-0.5f, -0.5f, 0.0f),
-			glm::vec3(0.5f, -0.5f, 0.0f),
-			glm::vec3(0.0f, 0.5f, 0.0f)
-		};
+		float w = 0.3f;
+		float h = 0.3f;
+		float d = 0.3f;
 
-		std::vector<unsigned int> indices{
-			0, 1, 2
-		};
+		//generate vertices
+		std::vector<float> positions({
+		 w,h,d,        // 0 (1,0,0) --> 0
+		w,h,d,       // 0 (0,1,0) --> 1
+		w,h,d,       // 0 (0,0,1) --> 2
+
+		w,h,-d,       // 1 (1,0,0) --> 3
+		w,h,-d,       // 1 (0,1,0) --> 4
+		w,h,-d,       // 1 (0,0,-1) --> 5
+
+		w,-h,d,       // 2 (1,0,0) --> 6
+		w,-h,d,       // 2 (0,-1,0)--> 7
+		w,-h,d,       // 2 (0,0,1) --> 8
+
+		w,-h,-d,       // 3 (1,0,0) --> 9
+		w,-h,-d,       // 3 (0,-1,0) --> 10
+		w,-h,-d,       // 3 (0,0,-1) --> 11
+
+		-w,h,d,       // 4 (-1,0,0) -->12
+		-w,h,d,       // 4 (0,1,0) --> 13
+		-w,h,d,       // 4 (0,0,1) --> 14
+
+		-w,h,-d,       // 5 (-1,0,0) --> 15
+		-w,h,-d,       //5 (0,1,0) --> 16
+		-w,h,-d,       // 5 (0,0-1) --> 17
+
+		-w,-h,d,       // 6 (-1,0,0) --> 18
+		-w,-h,d,       //6 (0,-1,0) --> 19
+		-w,-h,d,       // 6 (0,0,1) --> 20
+
+		-w,-h,-d,       // 7 (-1,0,0) --> 21
+		-w,-h,-d,       // 7 (0,-1,0) --> 22
+		-w,-h,-d, });
+
+		//generate indices
+		std::vector<unsigned int> indices({
+		   6,3,0, //(1,0,0)
 		
-		std::vector<glm::vec3> normals{
-			glm::vec3(-0.5f, -0.5f, 0.0f),
-			glm::vec3(0.5f, -0.5f, 0.0f),
-			glm::vec3(0.0f, 0.5f, 0.0f)
-		};
+		   6,9,3, //(1,0,0)
 
-		std::vector<glm::vec2> uvs{
-			glm::vec2(0, 0),
-			glm::vec2(0, 0),
-			glm::vec2(0, 0)
-		};
+		14,8,2, //(0,0,1)
 
-		VertexArray triangle = VertexArray(positions, indices, normals, uvs);
+		14,20,8, //(0,0,1)
+
+		19,22,7, //(0,-1,0)
+
+		7,22,10, //(0,-1,0)
+
+		18,12,21, //(-1,0,0)
+
+		12,15,21, //(-1,0,0)
+
+		13,1,16, //(0,1,0)
+
+		1,4,16, //(0,1,0)
+
+		5,11,17, //(0,0,-1)
+
+		23,17,11, //(0,0,-1) 
+			});
+
+		std::vector<float> normals({
+		1,0,0,
+		0,1,0,
+		0,0,1,
+
+		1,0,0,
+		0,1,0,
+		0,0,-1,
+
+		1,0,0,
+		0,-1,0,
+		0,0,1,
+
+		1,0,0,
+		0,-1,0,
+		0,0,-1,
+
+		-1,0,0,
+		0,1,0,
+		0,0,1,
+
+		-1,0,0,
+		0,1,0,
+		0,0,-1,
+
+		-1,0,0,
+		0,-1,0,
+		0,0,1,
+
+		-1,0,0,
+		0,-1,0,
+		0,0,-1
+			});
+
+
+		VertexArray cube = VertexArray(positions, indices, normals);
+		
 		while (m_Running) {
 			glClearColor(0.2f, 0.2f, 0.2f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			triangle.draw();
+			cube.draw();
 			/*
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);

95947bc1c8f79c04b23971b89791bfcf8944d3bb
Lollobin <e11909390@student.tuwien.ac.at>
2021-04-30 23:05:34 +0200

    hdr shader implementiert, wirkt etwas grau

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -17,8 +17,10 @@ public:
 		dynamicsWorld->setGravity(btVector3(0, -10, 0));
 
 		//setup rendering context
-		shader.reset(new TowerDelivery::Shader("assets/shader/vertex.glsl", "assets/shader/fragment.glsl"));
 		glEnable(GL_DEPTH_TEST);
+		shader.reset(new TowerDelivery::Shader("assets/shader/vertex.glsl", "assets/shader/fragment.glsl"));
+		hdrShader.reset(new TowerDelivery::Shader("assets/shader/hdr-vertex.glsl", "assets/shader/hdr-fragment.glsl"));
+		
 
 		//setup character
 		characterController = new TowerDelivery::CharacterController(0.5f, 0.5f, 60.0f, btVector3(0.0f, 3.0f, 0.0f), dynamicsWorld.get());
@@ -144,14 +146,40 @@ public:
 
 		gl_platform = new TowerDelivery::VertexArray(TowerDelivery::VertexArray::createCubeVertexArray(5.0f, 0.2f, 5.0f));
 
-		//setup textures
-		shader->setInt("material.diffuse", 0);
-		glActiveTexture(GL_TEXTURE0);
+		
 		
 		//load textures
 		tex_container = loadTexture("assets/textures/container_diffuse.png");
 		tex_pavement = loadTexture("assets/textures/pavement_diffuse.png");
 
+
+		//setup floating point framebuffer for hdr shader
+		glGenFramebuffers(1, &hdrFBO);
+		// create floating point color buffer
+		
+		glGenTextures(1, &colorBuffer);
+		glBindTexture(GL_TEXTURE_2D, colorBuffer);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, 1280.0f, 768.0f, 0, GL_RGBA, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// create depth buffer (renderbuffer)
+		
+		glGenRenderbuffers(1, &rboDepth);
+		glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1280.0f, 768.0f);
+		// attach buffers
+		glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "Framebuffer not complete!" << std::endl;
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		
+		//shader configuration
+		shader->Bind();
+		shader->setInt("material.diffuse", 0);
+		hdrShader->Bind();
+		hdrShader->setInt("hdrBuffer", 0);
 	}
 
 	void OnUpdate(TowerDelivery::Timestep ts) override {
@@ -160,8 +188,13 @@ public:
 		characterController->OnUpdate(ts);
 
 		//prepare for rendering
-		glClearColor(0.3f, 0.3f, 0.3f, 1);
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// 1. render scene into floating point framebuffer
+		glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		shader->Bind();
 		shader->setMat4("projection", projectionMatrix);
 		if (useDebugCamera) {
@@ -176,7 +209,7 @@ public:
 		//set directional lighting
 		shader->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
 		shader->setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-		shader->setVec3("dirLight.diffuse", 0.7f, 0.7f, 0.7f);
+		shader->setVec3("dirLight.diffuse", 0.1f, 0.1f, 0.1f);
 		shader->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
 
 		//set point lights
@@ -190,9 +223,9 @@ public:
 		shader->setFloat("pointLights[0].quadratic", 0.032);
 		
 
-		shader->setVec3("pointLights[1].position", 5.5f, 9.0f, -17.5f);
+		shader->setVec3("pointLights[1].position", -6.5f, 2.0f, -10.0f);
 		shader->setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-		shader->setVec3("pointLights[1].diffuse", 0.9f, 0.9f, 0.9f);
+		shader->setVec3("pointLights[1].diffuse", 1.0f, 1.0f, 1.0f);
 		shader->setVec3("pointLights[1].specular", 2.0f, 2.0f, 2.0f);
 		shader->setFloat("pointLights[1].constant", 1.0f);
 		shader->setFloat("pointLights[1].linear", 0.09);
@@ -254,6 +287,17 @@ public:
 		gl_floor->draw();
 
 		
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		
+		// 2. now render floating point color buffer to 2D quad and tonemap HDR colors to default framebuffer's (clamped) color range
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		hdrShader->Bind();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, colorBuffer);
+		hdrShader->setInt("hdr", true);
+		hdrShader->setFloat("exposure", 0.1f);
+		renderQuad();
+		
 		if (useDebugCamera)
 			camera->OnUpdate(ts);
 		else
@@ -294,6 +338,34 @@ public:
 		model = btScalar2mat4(btModelMatrix);
 
 		return model;
+	}
+
+	
+	void renderQuad()
+	{
+		if (quadVAO == 0)
+		{
+			float quadVertices[] = {
+				// positions        // texture Coords
+				-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+				-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+				 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+				 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+			};
+			// setup plane VAO
+			glGenVertexArrays(1, &quadVAO);
+			glGenBuffers(1, &quadVBO);
+			glBindVertexArray(quadVAO);
+			glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		}
+		glBindVertexArray(quadVAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
 	}
 
 
@@ -342,6 +414,13 @@ private:
 	bool useDebugCamera;
 	glm::mat4 projectionMatrix;
 	std::shared_ptr<TowerDelivery::Shader> shader;
+
+	std::shared_ptr<TowerDelivery::Shader> hdrShader;
+	unsigned int hdrFBO;
+	unsigned int colorBuffer;
+	unsigned int rboDepth;
+	unsigned int quadVAO = 0;
+	unsigned int quadVBO;
 
 	//character
 	TowerDelivery::CharacterController* characterController;
